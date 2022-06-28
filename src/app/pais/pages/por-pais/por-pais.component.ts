949e63d7c70e88a0f7708ead2db5b99eadf3e03b
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-06-28 10:26:17 -0500

    Creacion de Sugerencias

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,6 +5,14 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles:[
+    `
+    li{
+      cursor: pointer;
+    }
+    
+    `
+  ]
   
 })
 export class PorPaisComponent  {
@@ -13,12 +21,16 @@ export class PorPaisComponent  {
   hayError: boolean = false;
   paises  : Country [] = []
 
+  paisesSugeridos    : Country [] = []
+  mostrarsugerencias : boolean = false;
+
   constructor(private paisService: PaisService) { }
   
 
   buscar( termino: string ){
     this.hayError = false;
     this.termino = termino;
+    this.mostrarsugerencias = false
     
     
       this.paisService.buscarPais(this.termino)
@@ -39,6 +51,17 @@ export class PorPaisComponent  {
   }
   sugerencias(termino:string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarsugerencias = true;
     
+    this.paisService.buscarPais(termino)
+    .subscribe(
+      paises => this.paisesSugeridos = paises.splice( 0, 5 ),
+      (err) => this.paisesSugeridos = []
+      );
+  }
+
+  BuscarSugerido(termino: string){
+    this.buscar(termino);
   }
 }
